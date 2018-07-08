efa2db1c9382056a2f4895cafc403cd9569b3447
eraj2587 <e.raj2587@gmail.com>
2018-07-09 00:34:26 +0530

    Async await demonstration

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In Main");
+            Console.WriteLine($"This thread is {(Thread.CurrentThread.IsBackground ? "Background" : "Foreground")}" +
+                $" with ID {Thread.CurrentThread.ManagedThreadId}");
+            //------------------------------------------------------------------------------
+            //CallAsync();
+            /*Run on the same thread and callee stops for called method
+            ****OUTPUT****
+                In Main
+                This thread is Foreground with ID 1
+                Call Method
+                This thread is Foreground with ID 1
+                Existing Main
+             */
+            //------------------------------------------------------------------------------
+
+            Task.Run(async () =>
+            {
+                await CallAsync();
+            });
+            /*Run on different thread and callee wait for called method but contiue its execution 
+             and return its state when called method completed
+           ****OUTPUT****
+                In Main
+                This thread is Foreground with ID 1
+                Existing Main
+                Press any key to exit...
+                Call Method
+                This thread is Background with ID 3
+           */
+
+
+            Console.WriteLine("Existing Main");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static async Task CallAsync()
+        {
+            Thread.Sleep(5000);
+            await Console.Out.WriteLineAsync("Call Method");
+            Console.WriteLine($"This thread is {(Thread.CurrentThread.IsBackground ? "Background" : "Foreground")}" +
+                $" with ID {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
