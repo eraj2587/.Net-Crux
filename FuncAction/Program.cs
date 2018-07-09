b356abe7ce26723efd23c360db0ac43acf0d2178
eraj2587 <e.raj2587@gmail.com>
2018-07-09 23:54:03 +0530

    Use of Func

---
+++ FuncAction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncAction
+{
+    //https://msdn.microsoft.com/en-us/library/bb549151(v=vs.110).aspx
+    class Program
+    {
+        /*
+       Func<string, string> selector = 
+           (x) => 
+       {
+           return x.ToUpper();
+       };
+       */
+
+        static Func<string, string> selector = x => x.ToUpper();
+        
+        static void Main(string[] args)
+        {
+           var line= Console.ReadLine();
+            Console.WriteLine(selector(line));
+            Console.ReadKey();
+
+        }
+       
+            
+  }
+}
