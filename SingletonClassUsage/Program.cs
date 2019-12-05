d286888f0f416f66c9451db0924d84756f040688
Rajaram <rajaram.shelar2@westernunion.com>
2019-12-05 10:35:46 +0530

    singlton usage

---
+++ SingletonClassUsage/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingletonClassUsage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Singleton.GetSingleton();
+
+            Parallel.For(0, 30,
+                    i => {
+                        a.Echo(i);
+                    });
+            Console.ReadKey();
+        }
+    }
+
+    public class Singleton
+    {
+        static Singleton _instance = null;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetSingleton()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+
+        public void Echo(int a)
+        {
+            Thread.Sleep(10);
+            Console.WriteLine("echo : " + a.ToString());
+        }
+    }
+}
